6e07910dc553227e20d74d913fb72be06c789328
Mahmudulhasan162 <mahmudulhasanshafi2345@gmail.com>
2023-10-20 18:43:10 +0600

    users can add a product in the add product page but only if they are logged in

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Router from './Route/Router.jsx'
+import AuthProvider from './Components/Firebase/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={Router}></RouterProvider>
+    <AuthProvider><RouterProvider router={Router}></RouterProvider></AuthProvider>
   </React.StrictMode>,
 )
